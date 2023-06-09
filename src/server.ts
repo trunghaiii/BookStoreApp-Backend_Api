adb815912f2660b403fc271dc2c3ebba73835a92
trunghaiii <haitran022001@gmail.com>
2023-06-08 21:49:44 -0400

    fix bug being blocked by cors policy when call api from frontend

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 require('dotenv').config()
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+const cors = require('cors')
+
 import mongooseConnection from "./config/mongoose"
 import userRoute from "./routes/user"
 
 const app: Application = express();
 
 const PORT = process.env.PORT || 6969;
+
+//config to get req.body from client input
 app.use(bodyParser.urlencoded({ extended: true }));
+// config to fix being blocked by cors policy when call api from frontend
+app.use(cors());
 
 
 app.use('/api/v1/user', userRoute);
