08cb894bd480940d4a4cd04098ad3e2a0b4a51d8
trunghaiii <haitran022001@gmail.com>
2023-07-08 20:43:46 -0400

    modify get comment Api(not require login)

--- src/controller/book.ts
+++ src/controller/book.ts
@@ -801,31 +801,6 @@ export const postComment = async (req: express.Request, res: express.Response) =
 
 export const getComment = async (req: express.Request, res: express.Response) => {
 
-    // 0. verify access token
-    if (req.headers.authorization) {
-        //1. get access token sent from front end
-        let access_token = req.headers.authorization.split(' ')[1]
-
-        try {
-            // 2. verify accesstoken
-            const decoded = await jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY);
-
-        } catch (error) {
-            return res.status(401).json({
-                errorMessage: "Something wrong with your access token(invalid,expired,not exist...)",
-                errorCode: -1,
-                data: ""
-            })
-
-        }
-
-    } else {
-        return res.status(401).json({
-            errorMessage: "Something wrong with your access token(invalid,expired,not exist...)",
-            errorCode: -1,
-            data: ""
-        })
-    }
 
     // 1. get comment detail by Book Id
     let commentInfo: any = []
